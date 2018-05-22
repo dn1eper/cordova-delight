f5bae54eac7df34db8dec919bc4f882c997c9387
Andrey Z <dn6pr0@gmail.com>
2018-05-22 18:31:18 +0300

    По идее так можно отлаживать в браузере

--- client/js/index.js
+++ client/js/index.js
@@ -1,11 +1,13 @@
-HOST = "http://192.168.1.12/";
+//const HOST = "http://192.168.1.12/";
+const HOST = "localhost/"
+var app;
 
-document.addEventListener('deviceready', function() {
+function onLoad() {
+    app = angular.module('myCordovaApp', []);
     
-    var app = angular.module('myCordovaApp', []);
-    var post_id = 0;
-
     app.controller('post', ($scope, $http) => {
+        var post_id = 0;
+
         $scope.Next = () => $http.get(HOST + 'post.php?post_id='+ (++post_id))
         .then(res => $scope.post = res.data,
             err => console.error(err));
@@ -15,7 +17,23 @@ document.addEventListener('deviceready', function() {
             err => console.error(err));
     });
 
-    document.bind('swiperight', Next);
-    document.bind('swipeleft', Previous);
-}, false);
-    
+    document.addEventListener('swiperight', onSwipeRight, false);
+    document.addEventListener('swipeleft', onSwipeLeft, false);
+    document.addEventListener('onclick', onClick, false);
+}
+
+function onSwipeRight() {
+    post.innerHTML = "right";
+}
+
+function onSwipeLeft() {
+    post.innerHTML = "left";
+}
+
+function onClick() {
+    post.innerHTML = "click";
+}
+
+
+//document.addEventListener('deviceready', onLoad, false);
+document.addEventListener('onload', onLoad, false);
