473e6d9896d142f041b346007c5f8c5132eeb4aa
mrexox <mrexox@outlook.com>
2018-05-22 19:33:10 +0300

    Angular не работает

--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -1,23 +1,39 @@
 HOST = "http://192.168.1.12/server/";
 
-document.addEventListener('deviceready', function() {
-    
-    var app = angular.module('myCordovaApp', []);
+app.controller('post', ($scope, $http) => {
     var post_id = 0;
 
-    app.controller('post', ($scope, $http) => {
-        $scope.Next = () => $http.get(HOST + 'post.php?post_id='+ (++post_id))
-        .then(res => $scope.post = res.data,
-            err => console.error(err));
-        
-        $scope.Previous = () => $http.get(HOST + 'post.php?post_id='+ (--post_id))
-        .then(res => $scope.post = res.data,
-            err => console.error(err));
-    });
+    $scope.Next = () => $http.get(
+        HOST + 'post.php?post_id='+ (++post_id), {
+        headers: {'Access-Control-Allow-Origin' : "*"}} )
+    .then(res => $scope.post = res.data,
+        err => console.error(err));
 
-    document.getElemebtById('next-button').bind('click', Next);
-    document.bind('swiperight', Next);
-    document.bind('swipeleft', Previous);
+    $scope.Previous = () => $http.get(HOST + 'post.php?post_id='+ (--post_id), {
+        headers: {'Access-Control-Allow-Origin' : "*"}} )
+    .then(res => $scope.post = res.data,
+        err => console.error(err));
+});
 
-}, false);
-    
+/*
+function onLoad() {
+    document.addEventListener('swiperight', onSwipeRight, false);
+    document.addEventListener('swipeleft', onSwipeLeft, false);
+    document.addEventListener('onclick', onClick, false);
+}
+
+function onSwipeRight() {
+    post.innerHTML = "right";
+}
+
+function onSwipeLeft() {
+    post.innerHTML = "left";
+}
+
+function onClick() {
+    post.innerHTML = "click";
+}
+*/
+
+document.addEventListener('deviceready', onLoad, false);
+//document.addEventListener('onload', onLoad, false);
