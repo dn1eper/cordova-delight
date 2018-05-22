de86222b165477eea16ba17a5fe99bea4c5a7591
Andrey Z <dn6pr0@gmail.com>
2018-05-22 19:19:39 +0300

    Теперь все работает, можно перелистывать статьи кнопкой

--- client/js/index.js
+++ client/js/index.js
@@ -1,22 +1,24 @@
-//const HOST = "http://192.168.1.12/";
-const HOST = "localhost/"
-var app;
+const HOST = "http://192.168.1.12:/0/";
+//const HOST = "http://localhost:80/"
+var app = angular.module('app', []);
 
-function onLoad() {
-    app = angular.module('myCordovaApp', []);
+app.controller('post', ($scope, $http) => {
+    var post_id = 0;
+
+    $scope.Next = () => $http.get(
+        HOST + 'post.php?post_id='+ (++post_id), {
+        headers: {'Access-Control-Allow-Origin' : "*"}} )
+    .then(res => $scope.post = res.data,
+        err => console.error(err));
     
-    app.controller('post', ($scope, $http) => {
-        var post_id = 0;
+    $scope.Previous = () => $http.get(HOST + 'post.php?post_id='+ (--post_id), {
+        headers: {'Access-Control-Allow-Origin' : "*"}} )
+    .then(res => $scope.post = res.data,
+        err => console.error(err));
+});
 
-        $scope.Next = () => $http.get(HOST + 'post.php?post_id='+ (++post_id))
-        .then(res => $scope.post = res.data,
-            err => console.error(err));
-        
-        $scope.Previous = () => $http.get(HOST + 'post.php?post_id='+ (--post_id))
-        .then(res => $scope.post = res.data,
-            err => console.error(err));
-    });
-
+/*
+function onLoad() {
     document.addEventListener('swiperight', onSwipeRight, false);
     document.addEventListener('swipeleft', onSwipeLeft, false);
     document.addEventListener('onclick', onClick, false);
@@ -33,7 +35,7 @@ function onSwipeLeft() {
 function onClick() {
     post.innerHTML = "click";
 }
+*/
 
-
-//document.addEventListener('deviceready', onLoad, false);
-document.addEventListener('onload', onLoad, false);
+document.addEventListener('deviceready', onLoad, false);
+//document.addEventListener('onload', onLoad, false);
--- server/comment.php
+++ server/comment.php
@@ -2,6 +2,9 @@
 
 require_once 'db/model.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
 if (isset($_GET["post_id"])) {
     $post_id = $_GET["post_id"];    
     $model = new Model();
