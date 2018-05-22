8ab4b4c9dbe609d9cb4c6787e036cc4b94d92e7f
Andrey Z <dn6pr0@gmail.com>
2018-05-22 20:19:35 +0300

    Ограничил листание первой и последней страницей. Проверка свайпов.

--- client/js/index.js
+++ client/js/index.js
@@ -1,23 +1,34 @@
-const HOST = "http://192.168.1.12:/0/";
+const HOST = "http://192.168.1.8:80/";
 //const HOST = "http://localhost:80/"
+
 var app = angular.module('app', []);
 
 app.controller('post', ($scope, $http) => {
     var post_id = 0;
 
-    $scope.Next = () => $http.get(
-        HOST + 'post.php?post_id='+ (++post_id), {
-        headers: {'Access-Control-Allow-Origin' : "*"}} )
-    .then(res => $scope.post = res.data,
-        err => console.error(err));
-    
-    $scope.Previous = () => $http.get(HOST + 'post.php?post_id='+ (--post_id), {
-        headers: {'Access-Control-Allow-Origin' : "*"}} )
-    .then(res => $scope.post = res.data,
-        err => console.error(err));
+    $scope.Next = () => {
+        $http.get(
+            HOST + 'post.php?post_id='+ (++post_id), {
+            headers: {'Access-Control-Allow-Origin' : "*"}} )
+        .then(res => {
+                if (res.data.length == 0) post_id--;
+                else $scope.post = res.data;
+            },
+            err => console.error(err));
+    };
+    $scope.Next();
+
+    $scope.Previous = () => {
+        if (post_id <= 1) return;
+        $http.get(
+            HOST + 'post.php?post_id='+ (--post_id), {
+            headers: {'Access-Control-Allow-Origin' : "*"}} )
+        .then(res => $scope.post = res.data,
+            err => console.error(err));
+    };
 });
 
-/*
+
 function onLoad() {
     document.addEventListener('swiperight', onSwipeRight, false);
     document.addEventListener('swipeleft', onSwipeLeft, false);
@@ -35,7 +46,7 @@ function onSwipeLeft() {
 function onClick() {
     post.innerHTML = "click";
 }
-*/
+
 
 document.addEventListener('deviceready', onLoad, false);
-//document.addEventListener('onload', onLoad, false);
+//onLoad();
